31f6b4a06228d620ff7593b73f4869d541e09cc1
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-03-17 20:48:34 +0800

    init commit

--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "BabyBus/dao"
 
 func main() {
 	//
-	type demo struct {
-		Id  string
-		Pwd string
-	}
-	s := "{\"Id\":\"123\",\"Pwd\":\"123456\"}"
-	var d demo
-	err := json.Unmarshal([]byte(s), &d)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	}
-	fmt.Printf("id = %v, pwd = %v", d.Id, d.Pwd)
+	dao.InitMysql()
 
 }
