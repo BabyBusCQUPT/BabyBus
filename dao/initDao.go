64dd1e73f89c959e1d494b65c69bbc01edb012ec
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-03-17 20:48:47 +0800

    init commit

--- dao/initDao.go
+++ dao/initDao.go
@@ -1,1 +1,20 @@
 package dao
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func InitMysql() {
+	dns := "root:pwd@/dbname?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB = db
+}
