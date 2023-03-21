30d1488cd1fd3b8fb3f26e6710783c3fff7d86fe
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-03-21 19:21:02 +0800

    refactor:封装转化string转int的接口

---
+++ tool/judgeParament.go
@@ -0,0 +1,26 @@
+package tool
+
+import (
+	"BabyBus/config"
+	"strconv"
+)
+
+// IsValidAndTrans 判断字符串是否为空并且转化为int类型
+func IsValidAndTrans(s string) (int, error) {
+	if s == "" {
+		return config.InvalidParameter, config.InvalidParameterErr
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return config.InvalidParameter, err
+	}
+	return i, nil
+}
+
+// IsValid 判断字符串是否为空
+func IsValid(s string) error {
+	if s == "" {
+		return config.InvalidParameterErr
+	}
+	return nil
+}
