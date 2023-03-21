679475c06f9088ea0455c46acd2beb6815c2897e
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-03-21 19:19:55 +0800

    doc:配置文件

---
+++ config/err.go
@@ -0,0 +1,7 @@
+package config
+
+import "errors"
+
+var (
+	InvalidParameterErr = errors.New("invalid parameters")
+)
---
+++ config/user.go
@@ -0,0 +1,4 @@
+package config
+
+// InvalidParameter 参数无效返回值
+const InvalidParameter = -1
