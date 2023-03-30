033c70558e7e52da613e857dcd0d7fe638f30fd3
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-03-30 20:07:51 +0800

    fix:修复可重复绑定好友的bug

--- service/babyFriend.go
+++ service/babyFriend.go
@@ -12,21 +12,53 @@ func GetUserFriends(openid string) (babyFriend []model.BabyFriend, err error) {
 
 // BindFriend 请求绑定朋友
 func BindFriend(userId string, friendId string) error {
-	return dao.BindFriend(userId, friendId)
+	friend := model.User{
+		OpenId: friendId,
+	}
+	if err := dao.GetUserInfo(&friend); err != nil {
+		return err
+	}
+	if friend.ID == 0 {
+		return errors.New("用户邀请绑定的好友不存在")
+	}
+	user := model.User{
+		OpenId: userId,
+	}
+	if err := dao.GetUserInfo(&user); err != nil {
+		return err
+	}
+	if err := dao.BindFriend(userId, friendId); err != nil {
+		return err
+	}
+	SendMsg(userId, user.Nickname+"邀请您绑定好友关系")
+	return nil
 }
 
 func CountFriend(openId string) (int64, error) {
 	return dao.CountFriend(openId)
 }
 
-/*
 // AcceptFriend 绑定朋友被同意
 func AcceptFriend(userId string, friendId string) error {
-	return dao.AcceptFriend(userId, friendId)
+	if err := dao.AcceptFriend(userId, friendId); err != nil {
+		return err
+	}
+	SendMsg(userId, "绑定成功")
+	return nil
 }
 
 // RejectFriend 绑定朋友被拒绝
 func RejectFriend(userId string, friendId string) error {
-	return dao.RejectFriend(userId, friendId)
+	if err := dao.RejectFriend(userId, friendId); err != nil {
+		return err
+	}
+	SendMsg(userId, "绑定好友失败")
+	return nil
 }
-*/
+
+func SendMsg(userId string, msg string) {
+	rwLocker.RLock()
+	defer rwLocker.RUnlock()
+	node := config.ClientMap[userId]
+	node.DataQueue <- []byte(msg)
+}
