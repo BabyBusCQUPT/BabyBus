55bf0bcc558c835c9df81607239cf9fb01470938
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-04-03 20:03:18 +0800

    doc:定义消息

--- config/msg.go
+++ config/msg.go
@@ -2,4 +2,10 @@ package config
 
 import "BabyBus/model"
 
-var ClientMap = make(map[string]*model.Node, 0)
+var (
+	ClientMap = make(map[string]*model.Node, 0)
+	Yes       = "1" //同意好友申请
+	No        = "0" //拒绝好友申请
+	Rejected  = "已拒绝你的好友绑定申请"
+	Accepted  = "已通过你的好友绑定申请"
+)
