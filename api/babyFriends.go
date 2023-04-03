518e8563e9e1e5ca8efcd40478341448956e5899
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-04-03 20:10:05 +0800

    refactor:重构站内消息绑定好友的接口

--- api/babyFriends.go
+++ api/babyFriends.go
@@ -34,20 +34,40 @@ func DeriveFriend(ctx *gin.Context) {
 }
 
 func BindFriend(ctx *gin.Context) {
+	var err error
+	friend := &model.User{}
 	user := &model.User{}
-	friendId := ctx.PostForm("friendId")
-	err := tool.IsValid(friendId)
-	if err != nil {
+	friend.OpenId = ctx.PostForm("friendId") //之前申请绑定好友的用户的id
+	choice := ctx.PostForm("choice")
+	user.Token = ctx.GetHeader("token") //目前处理站内消息的用户的id
+
+	if err = tool.IsValid(friend.OpenId); err != nil {
 		tool.Failure(400, "绑定朋友失败：朋友id为空", ctx)
 		return
 	}
-	friend := &model.User{OpenId: friendId}
+	if err = tool.IsValid(choice); err != nil {
+		tool.Failure(400, "绑定朋友失败：选择为空", ctx)
+		return
+	}
+
+	if choice == config.No {
+		//拒绝好友绑定
+		//删除原来绑定记录
+		if err = service.RejectFriend(friend.OpenId, user.OpenId); err != nil {
+			tool.Failure(500, "服务器错误", ctx)
+			log.Printf("站内消息绑定好友失败：删除绑定记录失败")
+			return
+		}
+		service.SendMsg(friend.OpenId, config.Rejected)
+		tool.Success("successfully rejected", ctx)
+		return
+	}
+
 	if err = service.GetUserInfo(friend); err != nil {
 		log.Printf("未在表中查询到朋友，朋友未注册:%s\n", err)
 		tool.Failure(400, "未在表中查询到朋友，朋友未注册", ctx)
 		return
 	}
-	user.Token = ctx.GetHeader("token")
 	if err = service.GetIdFromToken(user); err != nil {
 		log.Printf("未从用户token中成功获取用户id:%s\n", err)
 		tool.Failure(400, "未成功从用户token中获取用户id", ctx)
@@ -63,22 +83,10 @@ func BindFriend(ctx *gin.Context) {
 		tool.Failure(400, "绑定好友数已达上限", ctx)
 		return
 	}
-	if err = service.BindFriend(user.OpenId, friendId); err != nil {
-		if err == config.AddHimself {
-			tool.Failure(400, config.AddHimself.Error(), ctx)
-			return
-		} else if err == config.RepeatedAdd {
-			tool.Failure(400, config.RepeatedAdd.Error(), ctx)
-			return
-		}
-		log.Printf("绑定朋友id失败a:%s\n", err)
-		tool.Failure(500, "服务器错误", ctx)
-		return
-	}
+	service.SendMsg(friend.OpenId, config.Accepted)
 	ctx.JSON(200, gin.H{
-		"userId":   user.OpenId,
-		"friendId": friendId,
-		"context":  friend.Nickname + "请求与您绑定好友关系",
+		"code": http.StatusOK,
+		"info": "绑定成功",
 	})
 }
 
@@ -143,4 +151,5 @@ func AddFriend(ctx *gin.Context) {
 		tool.Failure(500, "服务器错误", ctx)
 		return
 	}
+	service.SendMsg(user.OpenId, user.Nickname+"邀请您绑定好友关系")
 }
