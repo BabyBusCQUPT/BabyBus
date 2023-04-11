f23692f7785ca72fc427212bda2301c61491dc9a
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2023-04-11 20:26:56 +0800

    refactor:重构用户结构体

--- model/user.go
+++ model/user.go
@@ -4,15 +4,18 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	OpenId        string
-	SessionKey    string
-	Age           int
-	Gender        byte
-	Token         string
-	Nickname      string
-	Image         string
-	UserLongitude float32
-	UserLatitude  float32
-	MostUsed      int
-	Friend        uint
+	OpenId     string
+	SessionKey string
+	Age        int
+	Gender     byte
+	Token      string
+	Nickname   string
+	Image      string
+	MostUsed   int
+	Friend     uint
+}
+
+type Position struct {
+	Longitude float64
+	Latitude  float64
 }
